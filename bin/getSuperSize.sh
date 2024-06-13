e0b9ad2a373fc279b9c8be58453261353e9ed611
Bruce Teng <torai.d.l@outlook.com>
2024-06-13 17:37:19 +0800

    Update: Add stock super partition size for OnePlus8T and OnePlus9R

--- bin/getSuperSize.sh
+++ bin/getSuperSize.sh
@@ -1,26 +1,8 @@
 #!/bin/bash
 device_code=$1
 case $device_code in
-	#13 13Pro 13Ultra
-	FUXI | NUWA |ISHTAR) size=9663676416;;
-	#RedmiNote12Turbo |K60Pro |MIXFold
-	MARBLE |SOCRATES|BABYLON) size=9663676416;;
-	#Redmi Note 12 5G
-	SUNSTONE) size=9122611200;;
-	#PAD6Max
-	YUDI) size=11811160064;;
-
-	op8t) size=11811160064;;
+	OnePlus9R) size=9932111872;;
+	OnePlus8T) size=7516192768;;
 	#Others
 	*) size=9126805504;;
 esac
-echo $size
-
-#pipa 9126805504 |Pad6
-#liuqin 9126805504 |Pad6Pro
-#sunstone 9126805504 or 9122611200 |Note 12 5G
-#rembrandt 9126805504 |K60E
-#redwood 9126805504 |Note12ProSpeed
-#mondrian 9126805504 |K60
-#yunluo 9126805504 |RedmiPad
-#ruby 9126805504 |Note 12 Pro
